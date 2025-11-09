7701b1af639a78a3ae65a660caaad4ef6a5ef69d
blacspot <obwogemcbride73@gmail.com>
2025-11-09 20:30:35 +0300

    added a couple of changes

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+import { Link } from "react-router-dom"
+
+const Herom = () => {
+  return (
+    <div className='hero'>
+       <div className='hero-text'>
+  <h1> Streamline Software with Smart Bug Tracking </h1>
+  <p>
+    Report, track, and resolve software defects efficiently in a collaborative workspace
+    
+  </p>
+  <p>
+    Gain hands-on experience in managing issue lifecycles, applying Agile practices,
+    and improving QA and development workflows — all in one platform.
+  </p>
+  <p>Ready to get started?</p>
+  <Link to="/signup">
+    <button className='btn'>
+      Join Us <img src={dark_arrow} alt=""/>
+    </button>
+  </Link>
+</div>
+
+    </div>
+  )
+}
+
+export default Herom
---
+++ src/components/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Herom from './Hero/Hero'
+//import Programs from './Programs/Programs'
+//import Title from './Title/Title'
+//import About from './About/About'
+//import Campus from './Campus/Campus'
+//import Contact from './Contact/Contact'
+//import Footer from './Footer/Footer'
+
+
+const Home = () => {
+  return (
+     <div>
+        <Navbar/>
+        <Herom />
+        
+      
+    
+    </div>
+  )
+}
+
+export default Home
